aa8d528066e77ddddda64c903ec37da240497a57
Grace Waddell <gwaddel3@nd.edu>
2023-11-10 16:40:15 -0500

    Update 1

---
+++ Tutorial_11_For_Submission.R
@@ -0,0 +1,11 @@
+#Tutorial 11 For Submission
+
+#Import libraries and data
+library(ggplot2)
+library(cowplot)
+data <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/data.txt", sep = ',', header = TRUE)
+height_weight_data <- read.csv("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/Height and Weight Data.csv")
+
+#Exercise 1
+
+#Exercise 2
