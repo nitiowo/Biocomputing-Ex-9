ff96b74005bae7f986fd64f698a6854ec0f3d50d
Grace Waddell <gwaddel3@nd.edu>
2023-11-10 16:55:44 -0500

    Update 3

--- Tutorial_11_For_Submission.R
+++ Tutorial_11_For_Submission.R
@@ -6,7 +6,7 @@ library(cowplot)
 data <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/data.txt", sep = ',', header = TRUE)
 height_weight_data <- read.csv("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/Height and Weight Data.csv")
 
-#Exercise 1
+#Exercise 1 (similar to the scatter plot examples in class)
 ggplot(data = height_weight_data, 
        aes(x = Height.Inches., y = Weight.Pounds.)) +
   geom_point() +
@@ -16,4 +16,9 @@ ggplot(data = height_weight_data,
   theme_classic() +
   theme(legend.title=element_blank())
 
-#Exercise 2
+#Exercise 2 
+#Bar graph (similar to the bar plot example in today's tutorial)
+ggplot(data, aes(x = region, y = observations)) +
+  stat_summary(fun.y = mean,
+               geom = "bar") +
+  theme_bw()
