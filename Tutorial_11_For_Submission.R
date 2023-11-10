99db5f618e0d0198035cbb55563947af0f612a39
Grace Waddell <gwaddel3@nd.edu>
2023-11-10 16:47:18 -0500

    Update 2

--- Tutorial_11_For_Submission.R
+++ Tutorial_11_For_Submission.R
@@ -7,5 +7,13 @@ data <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomp
 height_weight_data <- read.csv("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/Height and Weight Data.csv")
 
 #Exercise 1
+ggplot(data = height_weight_data, 
+       aes(x = Height.Inches., y = Weight.Pounds.)) +
+  geom_point() +
+  xlab("Height (inches)") +
+  ylab("Weight (pounds)") +
+  stat_smooth(method="lm") +
+  theme_classic() +
+  theme(legend.title=element_blank())
 
 #Exercise 2
