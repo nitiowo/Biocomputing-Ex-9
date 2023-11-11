5f1893def830fed1948e48f50f147c1e65984bd7
Grace Waddell <gwaddel3@nd.edu>
2023-11-11 10:55:16 -0500

    Update 5

--- Tutorial_11_For_Submission.R
+++ Tutorial_11_For_Submission.R
@@ -7,9 +7,10 @@ data <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomp
 height_weight_data <- read.csv("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-9/Height and Weight Data.csv")
 
 #Exercise 1 (similar to the scatter plot examples in class)
+#This graph takes a moment to load because there are 25,000 observations.
 ggplot(data = height_weight_data, 
        aes(x = Height.Inches., y = Weight.Pounds.)) +
-  geom_point() +
+  geom_point(size = 0.25) +
   xlab("Height (inches)") +
   ylab("Weight (pounds)") +
   stat_smooth(method="lm") +
@@ -18,18 +19,24 @@ ggplot(data = height_weight_data,
 
 #Exercise 2 
 #Bar graph of the means of the four populations (similar to the bar plot example in today's tutorial)
-ggplot(data = data, aes(x = region, y = observations)) +
+plot2_1 <- ggplot(data = data, aes(x = region, y = observations)) +
   stat_summary(fun.y = mean,
                geom = "bar") +
   xlab("Region") +
   ylab("Average observation")
   theme_bw()
 #Scatter plot of all of the observations
-ggplot(data = data, 
+plot2_2 <- ggplot(data = data, 
        aes(x=region,y=observations)) +
   geom_point() +
   geom_jitter() +
   xlab("Region") +
   ylab("Observations") +
   theme_classic()
+#Put these two plots together to compare them
+fig1 <- plot_grid(plot2_1, plot2_2, 
+                  labels = c("A", "B"),
+                  rel_widths = c(0.75, 1),
+                  ncol = 2,
+                  nrow = 1)
 #Answer:
