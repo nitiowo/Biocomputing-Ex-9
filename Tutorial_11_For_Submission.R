c406ca46e7309a247f8ed951e13fb5c8dad0d530
Grace Waddell <gwaddel3@nd.edu>
2023-11-11 11:03:23 -0500

    Update 6 (final update)

--- Tutorial_11_For_Submission.R
+++ Tutorial_11_For_Submission.R
@@ -25,6 +25,7 @@ plot2_1 <- ggplot(data = data, aes(x = region, y = observations)) +
   xlab("Region") +
   ylab("Average observation")
   theme_bw()
+plot2_1
 #Scatter plot of all of the observations
 plot2_2 <- ggplot(data = data, 
        aes(x=region,y=observations)) +
@@ -33,10 +34,18 @@ plot2_2 <- ggplot(data = data,
   xlab("Region") +
   ylab("Observations") +
   theme_classic()
-#Put these two plots together to compare them
+plot2_2
+#Put these two plots together to compare them (as done in lecture)
 fig1 <- plot_grid(plot2_1, plot2_2, 
                   labels = c("A", "B"),
                   rel_widths = c(0.75, 1),
                   ncol = 2,
                   nrow = 1)
+fig1
 #Answer:
+#The two graphs do tell different stories: the bar graph shows how the 
+ #averages for each region compare to each other, whereas the scatter
+ #plot demonstrates the spread of the data within each region. For
+ #example, the scatter plot demonstrates that the observations from the
+ #southern region have a bimodal distribution, which the bar graph does
+ #not show.
