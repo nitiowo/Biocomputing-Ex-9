a218d775bca95f3e0fed53b73de8a07cef1ff457
Matthew Eleazar <meleazar@nd.edu>
2023-11-17 03:06:47 -0500

    billionaires dataset read and produce trendline

---
+++ exercise09.R
@@ -0,0 +1,28 @@
+# Exercise 09 
+# Matthew Eleazar
+
+## Part 1
+library(ggplot2)
+
+
+billionaires <- read.csv("billionaires.csv", header = TRUE)
+
+ggplot(data = billionaires,
+       aes(x = age, y = finalWorth)) +
+  geom_point() +
+  xlab("Age") +
+  ylab("Net Worth (in Billions of $s)") +
+  stat_smooth(method="lm") +
+  theme_classic()
+
+## Part 2
+# Bar plot
+data <- read.table("data.txt", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+means <- aggregate(data$observations, by = list(data$region), FUN = mean)
+
+colnames(means) <- c("Region", "MeanObservations")
+
+ggplot(data = means, aes(x = Region, y = MeanObservations)) +
+  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+  labs(x = "Region", y = "Mean Observations", title = "Mean Observations by Region")
