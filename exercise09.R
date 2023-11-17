814c31470af46cb8bb05c64303891bdfd1596178
Matthew Eleazar <meleazar@nd.edu>
2023-11-17 12:11:31 -0500

    explanation added

--- exercise09.R
+++ exercise09.R
@@ -31,3 +31,10 @@ ggplot(data, aes(x = region, y = observations)) +
   geom_jitter(width = 0.2, height = 0, alpha = 0.5) +
   labs(x = "Region", y = "Observations") +
   ggtitle("Observations by Region with Jitter")
+
+# The bar plot and the scatter plot both tell different stories because
+# the bar plot only tells you the differences in the mean number
+# of observations per region while the scatter plot incorporates into the visualization
+# the spread of the observations per region. As you can see, the north and south regions
+# have observation that clump up to around the same number while the east and west regions
+# have a wide spread of numbers of observations. 
